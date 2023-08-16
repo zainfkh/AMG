7fd6e907caec7ac291c3b6fb88b911c7ccd3b782
zainfkh <131717739+zainfkh@users.noreply.github.com>
2023-08-16 03:14:12 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,81 @@
-
+var tl=gsap.timeline();
+tl
+.from('.navbar',{
+    x:-100,
+    duration:1
+})
+.from('.part1',{
+    autoAlpha:0,
+    scale:1.5,
+    duration:1
+})
+gsap.from('.part3',{
+    autoAlpha:0,
+    duration:1,
+    scrollTrigger:{
+        trigger:".part3",
+        toggleActions: 'play none none reverse',
+        start:'top 50%'
+    }
+})
+gsap.from('.part4',{
+    autoAlpha:0,
+    x:100,
+    scrollTrigger:{
+        trigger:".part4",
+        start:'top 50%'
+    }
+})
+gsap.from('.part5',{
+    autoAlpha:0,
+    x:-100,
+    scrollTrigger:{
+        trigger:".part5",
+        toggleActions: 'play none none reverse',
+        start:'top 50%'
+    }
+})
+gsap.from('.part6',{
+    autoAlpha:0,
+    duration:1,
+    scrollTrigger:{
+        trigger:".part6",
+        toggleActions: 'play none none reverse',
+        start:'top 50%'
+    }
+})
+gsap.from('.part7',{
+    autoAlpha:0,
+    x:100,
+    scrollTrigger:{
+        trigger:".part7",
+        start:'top 50%'
+    }
+})
+gsap.from('.part8',{
+    autoAlpha:0,
+    x:-100,
+    scrollTrigger:{
+        trigger:".part8",
+        toggleActions: 'play none none reverse',
+        start:'top 50%'
+    }
+})
+gsap.from('.part9',{
+    autoAlpha:0,
+    scrollTrigger:{
+        trigger:".part9",
+        toggleActions: 'play none none reverse',
+        start:'top 50%'
+    }
+})
+gsap.from('.part12',{
+    autoAlpha:0,
+    scale:0.5,
+    duration:1,
+    scrollTrigger:{
+        trigger:".part12",
+        toggleActions: 'play none none reverse',
+        start:'top 50%'
+    }
+})
